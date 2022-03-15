45d5e24be72810be8cbb3f8c9152df3847b943b3
nicholasprussen <nicholasprussen@u.boisestate.edu>
2022-03-14 20:58:04 -0600

    Updated client env

--- seedoverflow-client/tailwind.config.js
+++ seedoverflow-client/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  purge: {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    options: {
+      safelist: [/^bg-/, /^text-/]
+    }
+  },
+  darkMode: 'class',
+  mode: 'jit',
   theme: { /** TODO: Add color palette */
     extend: {
       transitionProperty: {
@@ -10,9 +18,12 @@ module.exports = {
       }
     },
     colors: {
-      'dark-mode-dark-background': '#231F20',
-      'dark-mode-lighter-dark-background': '#363133',
-      'main-text-color': '#FFFFFA',
+      'dark-background': '#231F20',
+      'dark-lighter-background': '#363133',
+      'light-background': '#ffffff',
+      'light-darker-background': '#eeeeee',
+      'dark-text-color': '#FFFFFA',
+      'light-text-color': '#000000',
       'green-accent-color': '#2A7221',
       'purple-accent-color': '#998FC7',
       'blue-accent-color': '#788AA3'
